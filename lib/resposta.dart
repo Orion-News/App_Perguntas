068e4c891a14d9c6df9458748a2ad8951721aa3e
Geverson <geverson.wss@gmail.com>
2021-04-10 00:46:02 -0300

    changed color button

--- lib/resposta.dart
+++ lib/resposta.dart
@@ -13,7 +13,7 @@ class Resposta extends StatelessWidget {
       width: double.infinity,
       margin: EdgeInsets.all(10),
       child: RaisedButton(
-              color: Colors.deepPurple[400],
+              color: Colors.red.shade900,
               textColor: Colors.white,
               child: Text(texto),
               onPressed: quandoSelecionar,
